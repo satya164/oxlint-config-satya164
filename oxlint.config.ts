51ec9c08e5991a8c9b1b4b8b7894afec7eebe7f0
Satyajit Sahoo <satyajit.happy@gmail.com>
2026-03-15 00:29:59 +0100

    fix: publish compiled files

--- oxlint.config.ts
+++ oxlint.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'oxlint';
-import jest from './jest.ts';
-import react from './react.ts';
-import recommended from './recommended.ts';
-import typechecked from './typechecked.ts';
-import vitest from './vitest.ts';
+import jest from './configs/jest.ts';
+import react from './configs/react.ts';
+import recommended from './configs/recommended.ts';
+import typechecked from './configs/typechecked.ts';
+import vitest from './configs/vitest.ts';
 
 export default defineConfig({
   extends: [recommended, react, typechecked, vitest, jest],
